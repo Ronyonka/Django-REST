703ec91e215bb0851bd93789c3a4893802137b21
Ronyonka <rononyonka@gmail.com>
2019-03-30 20:47:59 +0300

    added google maps api

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.conf import settings
 import requests 
 
 def home(request):
     response = requests.get('http://ip-api.com/json/')
     geodata = response.json()
-    return render (request, 'home.html', {'ip':geodata['query'],'country':geodata['country']})
+    return render (request, 'home.html', {
+        'ip':geodata['query'],
+        'isp':geodata['isp'],
+        'region':geodata['regionName'],
+        'country':geodata['country'],
+        'latitude':geodata['lat'],
+        'longitude':geodata['lon'],
+        'api_key':'AIzaSyAlBzoeKIXEs88q_0XtwjNq9jsAnVRmE5A'
+    })
+
