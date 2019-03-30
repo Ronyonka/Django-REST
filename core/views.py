1a229618472cc4a0e8b2ea6febd7adef0ae8360e
Ronyonka <rononyonka@gmail.com>
2019-03-30 21:38:04 +0300

    added github search

--- core/views.py
+++ core/views.py
@@ -3,8 +3,13 @@ from django.conf import settings
 import requests 
 
 def home(request):
-    response = requests.get('http://ip-api.com/json/')
-    geodata = response.json()
+    is_cached = ('geodata' in request.session)
+
+    if not is_cached:
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+        response = requests.get('http://ip-api.com/json/')
+        request.session['geodata'] = response.json()
+    geodata = request.session['geodata']
     return render (request, 'home.html', {
         'ip':geodata['query'],
         'isp':geodata['isp'],
@@ -12,5 +17,14 @@ def home(request):
         'latitude':geodata['lat'],
         'longitude':geodata['lon'],
         'api_key':settings.GOOGLE_MAPS_API_KEY,
+        'is_cached': is_cached
     })
 
+def github(request):
+    user = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = 'https://api.github.com/users/%s' % username
+        response = requests.get(url)
+        user = response.json()
+    return render(request,'github.html', {'user':user})
